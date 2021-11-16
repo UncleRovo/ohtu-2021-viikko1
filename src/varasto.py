f2cfda8f76466186063c90279b4fa69a8b1eadad
Mikkel Leskelä <mikkel.leskela@mail.com>
2021-11-16 17:26:36 +0200

    Korjattiin pylintvirhe

--- src/varasto.py
+++ src/varasto.py
@@ -12,7 +12,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.#############################################################################################3333
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
